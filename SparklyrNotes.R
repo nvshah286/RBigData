8972bab88ad2ba1c008b677ea7514b55e3f5ba6b
nvshah286 <nvshah286@gmail.com>
2018-05-09 23:09:39 -0400

    updates

--- SparklyrNotes.R
+++ SparklyrNotes.R
@@ -3,6 +3,10 @@ library(nycflights13)
 library(ggplot2)
 library(sparklyr)
 library(microbenchmark)
+library(dbplyr)
+
+#  Reference Source (most of the code will be found here.)
+# http://spark.rstudio.com/dplyr/
 
 flights2 = nycflights13::flights
 
@@ -34,6 +38,39 @@ flights %>% group_by(month) %>% summarize(avgDelay = mean(dep_delay, na.rm =T)) 
 microbenchmark(flights %>% filter(dep_delay > 1000))
 microbenchmark(flights2 %>% filter(dep_delay > 1000))
 
+## look for more complex functions. 
+arrange(flights, desc(dep_delay))
+summarise(flights, mean_dep_delay = mean(dep_delay))
+
+## All these are instances are not loaded into R. these are lazy instances and evaluated at runtime only.
+c1 <- filter(flights, day == 17, month == 5, carrier %in% c('UA', 'WN', 'AA', 'DL'))
+c2 <- select(c1, year, month, day, carrier, dep_delay, air_time, distance)
+c3 <- arrange(c2, year, month, day, carrier)
+c4 <- mutate(c3, air_time_hours = air_time / 60)
+
+## this will actually copy the data into R memory.
+## only when you call the collect function explicitly, will it copy the data into memory.
+carrierhours <- collect(c4)
 
 
-  
+str(carrierhours) ## this will be an R data frame. 
+
+## performing pairwise t test
+with(carrierhours, pairwise.t.test(air_time, carrier))
+
+## ggplot will not work with spark tables.. the data needs to be loaded in R for plotting.
+ggplot(carrierhours, aes(carrier, air_time_hours)) + geom_boxplot()
+
+## or convert to data.frame without collecting at runtime to visualize
+ggplot(as.data.frame(c4), aes(carrier, air_time_hours)) + geom_boxplot()
+
+
+## Window functions
+
+bestworst <- flights %>%
+  group_by(year, month, day) %>%
+  select(dep_delay) %>% 
+  filter(dep_delay == min(dep_delay, na.rm = T) || dep_delay == max(dep_delay, na.rm = T))
+
+
+dbplyr::sql_render(bestworst)
